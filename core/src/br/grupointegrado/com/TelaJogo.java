0104722668d87243df9e3b6d96afe4a10c3cb770
robsoneufrasiostirle <robson_stirle@hotmail.com>
2015-08-10 21:02:52 -0300

    aula 3

--- core/src/br/grupointegrado/com/TelaJogo.java
+++ core/src/br/grupointegrado/com/TelaJogo.java
@@ -1,12 +1,15 @@
 package br.grupointegrado.com;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
@@ -25,6 +28,13 @@ public class TelaJogo extends TelaBase {
 
     private BitmapFont fonte;
     private Label lbpontuacao;
+    private Image jogador;
+    private Texture texturajogador;
+    private Texture texturajogadoresquerda;
+    private Texture texturajogadordireita;
+
+    private boolean indodireita;
+    private boolean indoesquerda;
 
     public  TelaJogo(MainGame game){
         super(game);
@@ -40,6 +50,7 @@ public class TelaJogo extends TelaBase {
 
         initfonte();
         initInformacao();
+        initJogador();
 
     }
 
@@ -64,8 +75,54 @@ public class TelaJogo extends TelaBase {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         lbpontuacao.setPosition(10, camera.viewportHeight - 20);
+
+        capturateclas();
+
+        atualizarJogador(delta);
+        
         palco.act(delta);
         palco.draw();
+    }
+    // atualiza a posição do jodador
+    private void atualizarJogador(float delta) {
+
+        // velocidade de movimento do jogador
+        float velocidade = 200;
+        if (indodireita){
+
+            if(jogador.getX() < camera.viewportWidth - jogador.getImageWidth() ){
+                float x = jogador.getX() + velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x,y);
+            }
+
+
+        }
+        if (indoesquerda){
+
+            if(jogador.getX() > 0  ) {
+                float x = jogador.getX() - velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+
+        }
+    }
+
+    // verifica quais teclas estao pressionadas
+    private void capturateclas() {
+
+        indodireita = false;
+        indoesquerda = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+
+            indoesquerda = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+
+            indodireita = true;
+        }
     }
 
     @Override
@@ -77,6 +134,18 @@ public class TelaJogo extends TelaBase {
     @Override
     public void pause(){
 
+    }
+
+    private void initJogador() {
+        texturajogador = new Texture("sprites/player.png");
+        texturajogadordireita = new Texture("sprites/player-right.png");
+        texturajogadoresquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturajogador);
+        float x = camera.viewportWidth /2 -jogador.getWidth() /2;
+        float y = 15;
+        jogador.setPosition(x, y);
+        palco.addActor(jogador);
     }
 
     @Override
@@ -91,6 +160,9 @@ public class TelaJogo extends TelaBase {
         batch.dispose();
         palco.dispose();
         fonte.dispose();
+        texturajogadordireita.dispose();
+        texturajogador.dispose();
+        texturajogadoresquerda.dispose();
     }
 
 
